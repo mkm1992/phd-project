bbf1ec5fdb84b7ce2d611d12589f802e518dddf6
mkm1992 <mokm1992@gmail.com>
2019-07-20 16:28:02 +0430

    code for 1 resource

--- codes/thirdPart/main2.m
+++ codes/thirdPart/main2.m
@@ -1,7 +1,7 @@
 clc
 clear all
-N_service = 10;
-N_server = 7;
+N_service = 4;
+N_server = 3;
 Dmax = 100;
 Rmax = 200;
 N_resource = 1; % cpu, ram, storage
@@ -17,7 +17,7 @@ for j = N_server:-1:1
     for i = N_service:-1:1
         if i1(i)==1
             rs = resourceShare(j)-demand_service(i);
-            if rs >= 0
+            if rs > 0
                 mappingVar(i,j) = 1;
                 i1(i) = 0;
                 resourceShare(j) = resourceShare(j)-demand_service(i);
@@ -27,23 +27,6 @@ for j = N_server:-1:1
 end
 disp(sum(resource_server))
 disp(sum(demand_service))
-sum_vector = demand_service'*mappingVar;
-ind = find(sum_vector>0);
 
-temp = zeros(1,N_server);
-for j= 1: N_server
-    if sum_vector(j)>0
-        temp(j)= 1;
-    end
-end
-i = ind(1);
-for j= 1: N_server
-    if sum_vector(i)< resource_server(j) & temp(j)==0
-        temp(i) = 0;
-        temp(j) = 1;
-        i = i+1;
-    end
-end    
-
-sum_vector
-resource_server'
+    
+    
