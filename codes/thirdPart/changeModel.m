622e052bda19d0a4feb6012f064bcb15154f5604
mkm1992 <mokm1992@gmail.com>
2019-07-28 12:23:05 +0430

    2 method

--- codes/thirdPart/changeModel.m
+++ codes/thirdPart/changeModel.m
@@ -1,31 +1,17 @@
-clc
-clear all
-%% define variables
-N_service = 10;
-N_server = 7;
-Dmax = [1000, 10];
-Rmax = [3000, 30];
-N_resource = 2; % cpu, ram, storage
-demand_service = rand(N_service,N_resource);
-resource_server = rand(N_server,N_resource);
-demand_service = demand_service.*Dmax; 
-resource_server = resource_server.*Rmax;
-
-%% sort variables
 weight = [1, 100];
 demand_service1 = sorting(demand_service, N_resource, weight, N_service);
 resource_server1 = sorting(resource_server, N_resource, weight, N_server);
 %% mapping
-mappingVar = zeros(N_service, N_server);
+mappingVar1 = zeros(N_service, N_server);
 i = N_service;
 mapStatus = ones(1,N_service);
 resourceShare = resource_server1(:,2:3);
 for j = N_server:-1:1
-    for i = 1:N_service
+    for i = N_service:-1:1
         if mapStatus(i)==1
             rs = resourceShare(j,:)-demand_service1(i,2:3);
             if all(rs >= 0) 
-                mappingVar(i,j) = 1;
+                mappingVar1(i,j) = 1;
                 mapStatus(i) = 0;
                 resourceShare(j,:) = resourceShare(j,:)-demand_service1(i,2:3);
             end
@@ -42,7 +28,7 @@ end
     for j = N_server:-1:1
         demand_service2(i,:) = -resourceShare1(j,:)+ demand_service2(i,:);
         indMap = find(resourceShare1(j,1)==resourceShare(:,1));
-        mappingVar(i,indMap) = 1;
+        mappingVar1(i,indMap) = 1;
         if all(demand_service2(i,:) <= 0)
             j = 0;
             break;
@@ -54,20 +40,20 @@ end
 %% remap again for special case 
 disp(sum(resource_server1(:,2:3)))
 disp(sum(demand_service1(:,2:3)))
-sum_vector = demand_service1(:,2:3)'*mappingVar;
-ind(1,:) = find(sum_vector(1,:)>0);
-ind(2,:) = find(sum_vector(2,:)>0);
-temp = zeros(1,N_server);
+sum_vector = demand_service1(:,2:3)'*mappingVar1;
+ind1(1,:) = find(sum_vector(1,:)>0);
+ind1(2,:) = find(sum_vector(2,:)>0);
+temp1 = zeros(1,N_server);
 for j= 1: N_server
     if all(sum_vector(:,j)>0)
-        temp(j)= 1;
+        temp1(j)= 1;
     end
 end
-i = ind(1,1);
+i = ind1(1,1);
 for j= 1: N_server
-    if all(sum_vector(:,i)< resource_server1(j,2:3)') && temp(j)==0
-        temp(i) = 0;
-        temp(j) = 1;
+    if all(sum_vector(:,i)< resource_server1(j,2:3)') && temp1(j)==0
+        temp1(i) = 0;
+        temp1(j) = 1;
         i = i+1;
     end
 end    
