51037e776a163e58a9768859ad7512fe6e76801d
mkm1992 <mokm1992@gmail.com>
2019-08-13 17:16:46 +0430

    UT MAP correct

--- codes/simple_one/MapPRB2UT.m
+++ codes/simple_one/MapPRB2UT.m
@@ -5,7 +5,7 @@ PRB_Numbermap = zeros(1,N_PRB);
 for j = 1:N_Ut
     indexUEPRB = find(AllPRB2UT(:,j)==1);
     [min_prb,ind] = min(PRB_Numbermap(indexUEPRB));
-    PRB_Numbermap(ind) = PRB_Numbermap(ind) + 1;
-    Ut_map(j,ind) = 1;
+    PRB_Numbermap(indexUEPRB(ind)) = PRB_Numbermap(indexUEPRB(ind)) + 1;
+    Ut_map(j,indexUEPRB(ind)) = 1;
     
 end
