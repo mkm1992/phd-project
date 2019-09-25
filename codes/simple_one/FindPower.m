fee29fe7315c95c0187e9b6be1f5f1400c4d0a84
mkm1992 <mokm1992@gmail.com>
2019-09-25 11:27:33 +0330

    add update variable 1

--- codes/simple_one/FindPower.m
+++ codes/simple_one/FindPower.m
@@ -2,9 +2,10 @@ counter_max = 100;
 lambda_r = ones(1,N_Ut)*Rt/10;
 mu = ones(1,N_Ut)*Pmax/100;
 Kr = ones(1,N_rrh)*(2^C_thresh * var_q)/10;
-tr = ones(1,N_Ut)*(1/(delay_max-mean(Delay_Slice))+lamda)/10;                                                                                     
+tr = ones(1,N_Ut)*(1/(delay_max-mean(Delay_Slice))+lamda)/10;   
+eta = zeros(1,count_power);
 for  count_power=1:counter_max
     for j=1:N_Ut
-        
+        x1 = 
     end
 end
