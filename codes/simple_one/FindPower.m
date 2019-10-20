f858ba866b288c4faa01c9aa0b7a81b4aaa936c0
mkm1992 <mokm1992@gmail.com>
2019-10-20 17:37:44 +0330

    enhance code

--- codes/simple_one/FindPower.m
+++ codes/simple_one/FindPower.m
@@ -1,6 +1,6 @@
-counter_max = 100;
+counter_max = 20;
 lambda_r = ones(1,N_Ut)*Rt;
-mu = ones(1,N_Ut)*Pmax;
+mu = ones(1,N_rrh)*Pmax;
 Kr = ones(1,N_rrh)*(2^C_thresh * var_q);
 tr = ones(1,N_Ut)*(1/(delay_max-mean(Delay_Slice))+lamda);   
 etha = zeros(1,counter_max);
@@ -9,17 +9,22 @@ Power_cnvg_Thr = 0.01;
 iter_max = 1;
 for iter =1 :iter_max 
     for  count_power=1:counter_max
-        etha_u = ( -mu + lambda_r + tr + 1) * BW / log(2);
+        etha_u = (  lambda_r + tr + 1) * BW / log(2);
         mm_u = sum((abs( ChannelGain .* PrecodingMat) .^2).' * (service2slice),2).';
         zetha_u = (BW * N0) + Intf.';
-        keisi_u =  sum(( Kr + etha(count_power))*(abs(PrecodingMat) .^2),1) ;
+        keisi_u =  sum(( mu + Kr + etha(count_power))*(abs(PrecodingMat) .^2),1) ;
         %if (sum(Power(count_power)-Power(count_power)) < Power_cnvg_Thr) 
             for i = 1: N_Ut                       
-                Power(count_power,i) = ((etha_u(i) .* mm_u(i)) - (zetha_u(i) .* keisi_u(i)))/(keisi_u(i) .* mm_u(i));  
+                Power(count_power,i) = ((etha_u(i) .* mm_u(i)) - (zetha_u(i) .* keisi_u(i)))/(keisi_u(i) .* mm_u(i));
+                Popt(i) = max(0,Power(count_power,i));
+                %Popt(i) = min (Pmax , Popt(i));
             end
-            Popt(:) = max(0,Power(count_power,:));
-            etha(count_power) = sum(rate_UE)/(sum(Power(count_power,:))+0.001);
+            run rate
+            run FronthaulCap
+            run FindDelay
+            etha(count_power) = sum(rate_UE)/(sum(Popt)+0.001);
             run update_var
+
 
         %end
 
