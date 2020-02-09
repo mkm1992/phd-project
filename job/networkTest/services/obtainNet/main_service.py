2425c50081a0b2bae1c96077388d7f9e53adf591
mkm1992 <mokm1992@gmail.com>
2020-02-09 16:19:04 +0330

    change code a bit

--- job/networkTest/services/obtainNet/main_service.py
+++ job/networkTest/services/obtainNet/main_service.py
@@ -7,18 +7,20 @@ import os
 
 
 class MainService:
-    def __init__(self, url, type):
-        self.url = url
-        self.type = int(type)
-        #self.thresh = int(thresh)
-    def start_clustering(self, socket):    
-        f = os.popen('ping 172.16.149.216')
-        now = f.read()
-        print(now)
-        print(type(now))
-        # with open("Output.txt", "w") as text_file:
-        #     text_file.write(now)
-        #################################################
-        serialized_data = json.dumps({"throughput": now})
-        socket.write_message(serialized_data)
+	def __init__(self, url, type):
+		self.url = url
+		self.type = int(type)
+		#self.thresh = int(thresh)
+	def start_clustering(self, socket):
+		str1 = "ping "+self.url;   
+		f = os.popen(str1)
+		now = f.read()
+		print(now)
+		print(type(now))
+		# with open("Output.txt", "w") as text_file:
+		#     text_file.write(now)
+		#################################################
+		a =2;
+		serialized_data = json.dumps({"throughput": a})
+		socket.write_message(serialized_data)
         
