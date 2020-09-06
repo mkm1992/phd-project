d5e96b5f66f7bef8922b6929f25573e0cb480724
mkm1992 <mokm1992@gmail.com>
2020-09-06 18:30:00 +0430

    add file

--- codes/simulEnd/simulation1/main.m
+++ codes/simulEnd/simulation1/main.m
@@ -2,17 +2,17 @@ clear all
 clc
 close all
 %% variables
-N_min = 2;
+N_min = 10;
 N_step = 2;
-N_max = 10;
+N_max = 18;
 counter_max = 1;
-iter_max = 200;
+iter_max = 100;
 numVar = N_min:N_step:N_max ;
 etha = zeros(length(numVar),iter_max,counter_max);
 etha1 = zeros(length(numVar),iter_max,counter_max);
 %% loops
 nr = 0;
-for N_var = N_min:N_step:N_max   
+for N_PRB = N_min:N_step:N_max   
     nr = nr +1;
     %N_rrh = N_var;
     for iter =1 :iter_max 
@@ -60,3 +60,18 @@ for N_var = N_min:N_step:N_max
     end
 end
 plot( N_min:N_step:N_max , mean(etha(:,:,end),2))
+%% plot without nan
+len1 = N_min:N_step:N_max ;
+a = zeros(1,length(len1));
+b = a;
+num = a; 
+for i =1:length(len1)
+    for j =1:iter_max
+        if isnan(etha(i,j)) == 0 
+            a(i) = a(j) + etha(i,j,end);
+            num(i)=  num(i) + 1;
+        end
+    end
+    b(i) = a(i)/num(i);
+end
+plot( N_min:N_step:N_max , b)
