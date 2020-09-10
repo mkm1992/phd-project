91ee649fa9841830d302301e83ea6f086a1bf472
mkm1992 <mokm1992@gmail.com>
2020-09-10 23:28:32 +0430

    abstract add to thesis

--- deeprl/RL/VNF_placement/service_knapsack_1DC/main_service.py
+++ deeprl/RL/VNF_placement/service_knapsack_1DC/main_service.py
@@ -17,7 +17,7 @@ learning_rate = 0.5
 gamma = 0.65
 
 ## 
-env1 = Env(np.array([4,5,3,1]), np.array([12])) 
+env1 = Env(np.array([4,5,3]), np.array([12])) 
 env1.available_state() 
 env1.state_define()
 env1.value_state()
@@ -28,14 +28,18 @@ for episode in range(episodes):
     state = 0 #Gets current game state
     done = False        #decides whether the game is over
     score = 0
- 
+    a1 = np.random.randint(0,12,3)
+    env1 = Env(a1, np.array([12]))
+    env1.available_state() 
+    env1.state_define()
+    env1.value_state()
     for k in range(max_steps):
         statePath[episode, k] = state
         # With the probability of (1 - epsilon) take the best action in our Q-table
         done = env1.done_action(state)
         if done:
             break
-        if episode > 2000: #np.random.uniform(0, 1) > epsilon: #episode > 1800: 
+        if episode > 2400: #np.random.uniform(0, 1) > epsilon: #episode > 1800: 
             action = np.argmax(Q[state, :])
         # Else take a random action
         else:
