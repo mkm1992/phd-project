944b6ef55af054b9ceff420f7bbe60532a7961ca
mkm1992 <mokm1992@gmail.com>
2020-09-13 22:42:57 +0430

    enviroment add

--- deeprl/RL/VNF_placement/dynamicServiceKnapsack/enviroment.py
+++ deeprl/RL/VNF_placement/dynamicServiceKnapsack/enviroment.py
@@ -15,7 +15,33 @@ class Env:
         self.Val2 = 1;  #value for second service 
         self.MaxS1 = 5;
         self.MaxS2 = 10;
-    def state(VNF1, VNF2, DC_rem):
+    def state_define(self):
+        self.state = self.DC_rem, self.VNF1, self.VNF2
+    def choose_action(self):
+        if self.VNF1 > 0:
+            ac1 = list(range(1,self.VNF1+1))*self.Val1
+        else :
+            ac1 = list(range(-1,self.VNF1-1,-1))*self.Val1  
+        if self.VNF2 > 0:
+            ac2 = list(range(1,self.VNF2+1))*self.Val2
+        else :
+            ac2 = list(range(-1,self.VNF2-1,-1))*self.Val2 
+        actions = ac1, ac2
+        return actions
+    def next_state(self, action):
+        DC_remN = self.DC_rem -  action[0] - action[1]
+        return DC_remN
+    def reward(self, DC_remN):
+        if DC_remN < 0:
+            reward_s =  -100
+        else:
+            reward_s = pow(self.MaxS1*self.Val1 + self.MaxS2*self.Val2-DC_remN,2)
+        return reward_s
+        
+        
+
+        
+                       
         
         
 
