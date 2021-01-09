e5974578dbd1a6aa324e0b5f3c4856fb1116980a
mkm1992 <mokm1992@gmail.com>
2021-01-09 17:10:06 +0330

    matlab code start

--- Code_journal1/version1/main_simple.m
+++ Code_journal1/version1/main_simple.m
@@ -1,7 +1,8 @@
+clear all
+clc
+%% Varibale
 R = 500;
-S1 = 1;
-S2 = 0;
-S = S1 + S2;
+S = 1;
 BW = 120*1e3;%10*1e6;  
 n0 = -174;%-174 ; %dbm
 N0 = db2pow(n0)/1000;
@@ -10,12 +11,13 @@ Pc  = db2pow(Pc1)/1000;
 Pt = 23;
 Pmax = db2pow(Pt)/1000;
 Rt = .1*BW;%0.1*BW;
-%%
-N_U1 = randi(10,1);
-N_U2 = 0;
-ChannelGain = zeros(N_rrh,N_Ut);
-for i=1:N_rrh
-    for j=1:N_Ut
+%% Channel Gain
+N_UE = randi(10,1);
+Popt = ones(1,N_UE)*Pmax;
+N_RU =  4;
+ChannelGain = zeros(N_RU,N_UE);
+for i=1:N_RU
+    for j=1:N_UE
        distance(i,j)= randi(R,1,1);
        if distance(i,j)==0
            distance(i,j) = randi(R,1,1);
@@ -26,4 +28,12 @@ for i=1:N_rrh
        ChannelGain(i,j) = 100*(loss(i,j)^0.5)/sqrt(2)*(randn(1)+1i*randn(1));
     end
 end
-ChannelGain2 = ChannelGain;
+%% Rate
+rate_UE = zeros(1,N_UE);
+admission_UE = zeros(1,N_UE);
+RU_UE = zeros(N_RU,N_UE);
+for i = 1:N_UE
+    for j = 1:N_RU
+        rate_UE(i) = rate_UE(i) + BW* log2(1 + (Popt(i)*abs((ChannelGain(j,i).*RU_UE(j,i)))^2)/(BW*N0))*admission_UE(i);
+    end
+end
