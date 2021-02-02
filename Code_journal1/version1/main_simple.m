7f31f1fc079fe8ec4140f0a7138e95df61d9f6e4
mkm1992 <mokm1992@gmail.com>
2021-02-02 15:59:30 +0330

    enhace code

--- Code_journal1/version1/main_simple.m
+++ Code_journal1/version1/main_simple.m
@@ -41,23 +41,11 @@ for j=1:N_UE
     RU_UE(RU2UE,j) = 1;
 end
 %% UE Admission
-rate_UE = zeros(1,N_UE);
+rate_UE_min = zeros(1,N_UE);
 admission_UE = ones(1,N_UE);
 admission_UE1 =  zeros(1,N_UE);
 %RU_UE = zeros(N_RU,N_UE);
-for i = 1:N_UE
-    for j = 1:N_RU
-        rate_UE(i) = rate_UE(i) + BW* log2(1 + (Popt(i)*abs((ChannelGain(j,i).*RU_UE(j,i)))^2)/(BW*N0));
-    end
-end
-C_Fr = sum(rate_UE);
-Rate_fr_max = ceil(sum(rate_UE)*2/3);
-[best admission_UE1] = knapsack(ceil(rate_UE), admission_UE, Rate_fr_max);
-disp(best)
-items = find(admission_UE1);
-disp(items)
-%% RU Association
-rate_UE = zeros(1,N_UE);
+rate_UE_min = zeros(1,N_UE);
 Intf = zeros(1,N_UE);
 for i = 1:N_UE
     for j = 1:N_RU
@@ -66,25 +54,54 @@ for i = 1:N_UE
                 Intf(i) = Intf(i) + Popt(t)*abs(ChannelGain(j,i))^2 * (1 - RU_UE(j,i));
             end
         end
-        rate_UE(i) = rate_UE(i) + BW* log2(1 + (Popt(i)*abs((ChannelGain(j,i).*RU_UE(j,i)))^2)/(Intf(i)+BW*N0));
+        rate_UE_min(i) = rate_UE_min(i) + BW* log2(1 + (Popt(i)/100*abs((ChannelGain(j,i).*RU_UE(j,i)))^2)/(Intf(i)+BW*N0));
     end
 end
+C_Fr = sum(rate_UE_min);
+Rate_fr_max = ceil(sum(rate_UE_min)*2/3);
+[best admission_UE1] = knapsack(ceil(rate_UE_min), admission_UE, Rate_fr_max);
+disp(best)
+items = find(admission_UE1);
+disp(items)
+%% RU Association
+% rate_UE_min = zeros(1,N_UE);
+% Intf = zeros(1,N_UE);
+% for i = 1:N_UE
+%     for j = 1:N_RU
+%         for t = 1 : N_UE
+%             if i~=t 
+%                 Intf(i) = Intf(i) + Popt(t)*abs(ChannelGain(j,i))^2 * (1 - RU_UE(j,i));
+%             end
+%         end
+%         rate_UE_min(i) = rate_UE_min(i) + BW* log2(1 + (Popt(i)/20*abs((ChannelGain(j,i).*RU_UE(j,i)))^2)/(Intf(i)+BW*N0));
+%     end
+% end
 %%%%%%%%%% 
 C_Fr1 = C_Fr/(N_RU);
 Capacity_RU = ones(1, N_RU)*C_Fr1;
 Capacity_RU(1) = Capacity_RU(1)*2;
 Capacity_RU(2) = Capacity_RU(2)*1.2;
-[Sort_rate, I] = sort(rate_UE,'descend');
+[Sort_rate, I] = sort(rate_UE_min,'descend');
 sum(Capacity_RU)
-sum(rate_UE)
+sum(rate_UE_min)
 RU_UE = zeros(N_RU,N_UE);
 for j = 1:N_RU
-    for i = 1:N_UE
-        if Capacity_RU(j)> Sort_rate(i) && sum(RU_UE(:,i))==0
+    for i = 1:N_UE 
+        if Capacity_RU(j)> Sort_rate(i) && sum(RU_UE(:,i))==0 && admission_UE1(i)==1
             Capacity_RU(j) = Capacity_RU(j) - Sort_rate(i);
             RU_UE(j,i) = 1;
         end
     end
 end
 %% PRB and power allocation
-
+cvx_begin
+    variable Popt(1,N_UE)
+    maximize error
+    subject to
+    Prrh(:) <= Pmax;
+    %Prrh(:) <= 2^C_thresh * var_q *100;
+    Popt(:) > 0; 
+    Popt(:) < Pmax/10;
+    rate_UE(:) > Rt*1000;  
+    rate_UE(:) >1/(delay_max-mean(Delay_Slice1+Delay_Slice2))
+cvx_end
