35c76612c35bc75d4e5dd03e0586cec09b58d5e1
mkm1992 <mokm1992@gmail.com>
2021-06-23 15:57:59 +0430

    add algorithm

--- Code_journal1/version9/simulation_1/main_UE_Const.m
+++ Code_journal1/version9/simulation_1/main_UE_Const.m
@@ -1,9 +1,9 @@
 clear all
 clc
 run var_const
-count_min1 = 2;
+count_min1 = 3;
 count_step1 = 2;
-count_max1 = 12;
+count_max1 = 3;
 numvar = length(count_min1:count_step1:count_max1);
 iter_max = 1;
 sumRate = zeros(numvar,iter_max);
