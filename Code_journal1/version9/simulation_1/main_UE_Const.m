8439f81b3c616a02cd24efcfb213469a4c712af3
mkm1992 <mokm1992@gmail.com>
2021-07-07 12:49:03 +0430

    add figure to journal

--- Code_journal1/version9/simulation_1/main_UE_Const.m
+++ Code_journal1/version9/simulation_1/main_UE_Const.m
@@ -3,9 +3,9 @@ clc
 run var_const
 count_min1 = 3;
 count_step1 = 2;
-count_max1 = 3;
+count_max1 = 10;
 numvar = length(count_min1:count_step1:count_max1);
-iter_max = 1;
+iter_max = 3;
 sumRate = zeros(numvar,iter_max);
 %% initializing
 number_check = 0;
