46d41281a3d58964169e30edc4d254e5a630974a
mkm1992 <mokm1992@gmail.com>
2021-11-08 16:54:12 +0330

    Add a bit code

--- Code_journal2/enviroment.py
+++ Code_journal2/enviroment.py
@@ -6,13 +6,22 @@ created on Saturday Nov 6 11:06:00 2021
 import numpy as np
 
 class enviroment:
-    def __init__(self, Slice_Num, N_PRB) -> None:
-        self.Slice_Num = Slice_Num  # number of slices (a1,a2)
+    def __init__(self, Slice_URLLC, Slice_eMBB, N_PRB, N_VNF) -> None:
+        self.Slice_Num = Slice_URLLC + Slice_eMBB  # number of slices (a1,a2)
+        self.Slice_URLLC = Slice_URLLC  # number of URLLC slices
+        self.Slice_eMBB = Slice_eMBB  # number of eMBB slices
         self.VNF_List = [] # list of VNFs
+        self.N_VNF = N_VNF
         self.N_PRB = N_PRB
-        self.N_Slice = Slice_Num[0] + Slice_Num[1]
-        self.PRB_Slice  = np.zeros(self.N_PRB, self.N_Slice) # list of PRBs
-
+        self.PRB_Slice  = np.zeros((self.N_PRB, self.N_Slice)) # list of PRBs
+        self.VNF = np.zeros((1, self.N_Slice)) # list of VNFs
+        self.Rmin = np.zeros((1, self.N_Slice)) # list of Rmin
+        self.delay = np.zeros((1, self.N_Slice)) # list of delay
+        self.packetDrop = np.zeros((1, self.N_Slice)) # list of packet drop
+        
     def state(self):
-        return self.VNF_List        
+        return  [self.Rmin, self.delay, self.packetDrop]   
+
+    def action(self, action):
+        return action    
 
